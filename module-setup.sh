479a8d4d0b241bc0fc0a4d750ffac3ca1c3a7b8d
Nick Davidson <86747615+ndavidson-hpe@users.noreply.github.com>
2023-09-20 14:26:30 -0600

    Create tpm-provisioner directory

--- module-setup.sh
+++ module-setup.sh
@@ -26,6 +26,7 @@ install() {
   inst /etc/tpm-provisioner/blobs.conf
   inst /etc/tpm-provisioner/client.conf
   inst /opt/cray/cray-spire/tpm-provisioner-client
+  mkdir -p /var/lib/tpm-provisioner/
   inst /usr/bin/curl
   inst /usr/bin/jq
   inst /usr/bin/killall
